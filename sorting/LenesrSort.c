4d6305ab4c3f74783b10f3ab6c90b17a57b83d7b
GihanSangeeth <wmgsangeeth@gmail.com>
2018-10-31 14:56:01 +0530

    added lenear sort file

---
+++ sorting/LenesrSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int arr[6]={5,9,12,3,36,28};
+    printf("\nEnter the key to search : ");
+    int num,x,found;
+    scanf("%d",&num);
+    for(x=0;x<6;x++)
+    {
+        if(num ==arr[x]  )
+        {
+            printf("\nkey has found in %d index",x);
+	    found=1;		
+	    break;
+        }
+       else{continue;}
+
+    }
+
+    if(found!=1)
+    {
+        printf("Search not found!");
+    }
+    return 0;
+}
